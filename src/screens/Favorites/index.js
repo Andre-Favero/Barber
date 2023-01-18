aba277970fa1b5c1a799a6e60ab9423b51f60334
André Favero <andrefaverofacul@gmail.com>
2023-01-18 08:41:50 -0300

    feat: tela de barbeiro favoritos
    Nessa tela o usuário poderá ver os profissionais que ele marcou como seus favoritos, assim facilitando na busca pelo mesmo

--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -1,6 +1,47 @@
-import React from 'react';
-import {Container} from './styles';
+import React, {useState, useEffect} from 'react';
+import {Container, HeaderArea, HeaderTitle, Scroller, ListArea} from './styles';
+import BarberItem from '../../components/BarberItem';
+import api from '../../api';
+import {Alert, RefreshControl} from 'react-native';
 
 export default () => {
-  return <Container />;
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  const getFavorites = async () => {
+    setLoading(true);
+    setList([]);
+
+    let res = await api.getFavorites();
+    if (res.error === '') {
+      setList(res.list);
+    } else {
+      Alert.alert('Error ' + res);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <Container>
+      <HeaderArea>
+        <HeaderTitle>Favoritos</HeaderTitle>
+      </HeaderArea>
+
+      <Scroller
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={getFavorites} />
+        }>
+        <ListArea>
+          {list.map((item, k) => (
+            <BarberItem key={k} data={item} />
+          ))}
+        </ListArea>
+      </Scroller>
+    </Container>
+  );
 };
--- src/screens/Favorites/styles.js
+++ src/screens/Favorites/styles.js
@@ -1,3 +1,64 @@
 import styled from 'styled-components';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #111;
+`;
+export const HeaderArea = styled.View`
+  height: 50px;
+  justify-content: center;
+  padding: 0 20px;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 0 20px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 30px;
+`;
+
+export const ListArea = styled.View`
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const WarningTextEmptyList = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  align-items: flex-start;
+`;
+export const WarningTextEmptyListBold = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+  align-items: flex-start;
+`;
+
+export const ImagePreloadArea = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+export const ImagePreload = styled.Image`
+  width: 130%;
+  height: 270px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 154px;
+`;
+export const ImagePreloadlarge = styled.Image`
+  width: 100%;
+  height: 350px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 120px;
+`;
