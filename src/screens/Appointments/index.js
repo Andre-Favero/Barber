0ec7ea288c355b3801753b4c7fa3dcb427210df1
André Favero <andrefaverofacul@gmail.com>
2023-01-18 14:51:20 -0300

    feat: tela de agendamentos
    (apenas demonstrativo)

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useState, useEffect} from 'react';
+import {
+  Container,
+  Scroller,
+  ListArea,
+  ImagePreloadlarge,
+  BodyText,
+} from './styles';
+import AppointmentItem from '../../components/AppointmentItem';
+import api from '../../api';
+import {Alert, RefreshControl} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
 
 export default () => {
+  const isFocused = useIsFocused();
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+  const [appointmentsEmpty, setAppointmentsEmpty] = useState(false);
+
+  useEffect(() => {
+    getAppointments();
+  }, [isFocused]);
+
+  const getAppointments = async () => {
+    setLoading(true);
+    setList([]);
+
+    let res = await api.getAppointments();
+    if (res.error === '') {
+      setList(res.list);
+      res.list.length > 0
+        ? setAppointmentsEmpty(false)
+        : setAppointmentsEmpty(true);
+    } else {
+      Alert.alert('Error ' + res);
+    }
+    setLoading(false);
+  };
+
   return (
     <Container>
-      <Text>Appointments</Text>
+      <Scroller
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={getAppointments} />
+        }>
+        {appointmentsEmpty && (
+          <>
+            <ImagePreloadlarge
+              source={require('../../assets/calendarEmpty.png')}
+            />
+            <BodyText>Sem agendamentos</BodyText>
+          </>
+        )}
+        <ListArea>
+          {list.reverse().map((item, k) => (
+            <AppointmentItem key={k} data={item} />
+          ))}
+        </ListArea>
+      </Scroller>
     </Container>
   );
 };
--- src/screens/Appointments/styles.js
+++ src/screens/Appointments/styles.js
@@ -1,3 +1,52 @@
 import styled from 'styled-components';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #111;
+`;
+
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 0 20px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 30px;
+`;
+
+export const ListArea = styled.View`
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const WarningTextEmptyList = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  align-items: flex-start;
+`;
+export const WarningTextEmptyListBold = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+  align-items: flex-start;
+`;
+export const BodyText = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+`;
+
+export const ImagePreloadArea = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+export const ImagePreloadlarge = styled.Image`
+  width: 70%;
+  height: 300px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 120px;
+  margin-left: 55px;
+`;
