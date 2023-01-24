c9f35111fe1ff15fc9aa4168fd653c3d2ad8d6ac
André Favero <andrefaverofacul@gmail.com>
2023-01-24 08:49:10 -0300

    feat: tela notícias

---
+++ src/screens/Noticias/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Container, ImagePreloadlarge, TextTittle, BackButton} from './styles';
+import {useNavigation} from '@react-navigation/native';
+import BackIcon from '../../assets/back.svg';
+
+export default () => {
+  const navigation = useNavigation();
+
+  const handleBackButton = () => {
+    navigation.goBack();
+  };
+  return (
+    <Container>
+      <ImagePreloadlarge source={require('../../assets/notice.png')} />
+      <TextTittle>Nenhuma notícia</TextTittle>
+      <BackButton underlayColor="transparent" onPress={handleBackButton}>
+        <BackIcon width="44" height="44" fill="#fff" />
+      </BackButton>
+    </Container>
+  );
+};
