9bc2d25f69b3f3eb70ceb3e9cb9b8ccb5fd083a4
André Favero <andrefaverofacul@gmail.com>
2023-01-24 17:38:22 -0300

    Feat: Finalização da tela equipe
     - requisições da API
    - marcações de loading

--- src/screens/Equipe/index.js
+++ src/screens/Equipe/index.js
@@ -1,21 +1,70 @@
-import React from 'react';
-import {Container, ImagePreloadlarge, TextTittle, BackButton} from './styles';
-import {useNavigation} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {
+  Container,
+  ImagePreloadlarge,
+  TextTittle,
+  BackButton,
+  HeaderArea,
+  HeaderTitle,
+  ListArea,
+  LoadingIcon,
+} from './styles';
+import {useIsFocused, useNavigation} from '@react-navigation/native';
 import BackIcon from '../../assets/back.svg';
+import BarberDesc from '../../components/BarberDesc';
+import api from '../../api';
+import {Alert} from 'react-native';
 
 export default () => {
+  const isFocused = useIsFocused();
+  const [locationText, setLocationText] = useState('');
+  const [listBarbers, setListaBarbers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const navigation = useNavigation();
 
   const handleBackButton = () => {
     navigation.goBack();
   };
-  return (
+
+  useEffect(() => {
+    getBarbers();
+  }, [isFocused]);
+
+  const getBarbers = async () => {
+    setListaBarbers([]);
+    let lat = null;
+    let lng = null;
+
+    let res = await api.getBarbers(lat, lng, locationText);
+    if (res.error === '') {
+      if (res.loc) {
+        setLocationText('');
+      }
+      setListaBarbers(res.data);
+      setLoading(false);
+    } else {
+      Alert.alert(res.error);
+    }
+  };
+
+  return !loading ? (
     <Container>
-      <ImagePreloadlarge source={require('../../assets/team.png')} />
-      <TextTittle>Nenhum funcionário</TextTittle>
-      <BackButton underlayColor="transparent" onPress={handleBackButton}>
-        <BackIcon width="44" height="44" fill="#fff" />
-      </BackButton>
+      {/* <ImagePreloadlarge source={require('../../assets/team.png')} /> */}
+      <HeaderArea>
+        <BackButton underlayColor="transparent" onPress={handleBackButton}>
+          <BackIcon width="44" height="44" fill="#fff" />
+        </BackButton>
+        <HeaderTitle>EQUIPE</HeaderTitle>
+      </HeaderArea>
+
+      <ListArea>
+        {listBarbers.map((item, k) => (
+          <BarberDesc key={k} data={item} />
+        ))}
+      </ListArea>
     </Container>
+  ) : (
+    <LoadingIcon size="large" color="#FFFFFF" />
   );
 };
