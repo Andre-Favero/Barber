d5c6b69b2925bf25240d0c845cf78209bf5b3f64
André Favero <andrefaverofacul@gmail.com>
2023-01-24 17:23:59 -0300

    feat: Outro caminho para fazer um agendamento

--- src/screens/Agendar/index.js
+++ src/screens/Agendar/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import {Container, ImagePreloadlarge, TextTittle, BackButton} from './styles';
+import React, {useEffect, useState} from 'react';
+import {
+  Container,
+  ImagePreloadlarge,
+  TextTittle,
+  BackButton,
+  HeaderArea,
+  HeaderTitle,
+  ListArea,
+  LoadingIcon,
+} from './styles';
 import {useNavigation} from '@react-navigation/native';
 import BackIcon from '../../assets/back.svg';
-
+import BarberItem from '../../components/BarberItem';
+import api from '../../api';
+import {Alert} from 'react-native';
 export default () => {
+  const [locationText, setLocationText] = useState('');
+  const [listBarbers, setListaBarbers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
 
   const handleBackButton = () => {
     navigation.goBack();
   };
+
+  useEffect(() => {
+    setLoading(true);
+    const getBarbers = async () => {
+      setListaBarbers([]);
+      let lat = null;
+      let lng = null;
+
+      let res = await api.getBarbers(lat, lng, locationText);
+      if (res.error === '') {
+        if (res.loc) {
+          setLocationText('');
+        }
+        setListaBarbers(res.data);
+      } else {
+        Alert.alert(res.error);
+      }
+      setLoading(false);
+    };
+    getBarbers();
+  }, [locationText]);
+
   return (
     <Container>
-      <TextTittle>Nenhum agendamento</TextTittle>
+      {/* <ImagePreloadlarge source={require('../../assets/team.png')} /> */}
+      <HeaderArea>
+        <BackButton underlayColor="transparent" onPress={handleBackButton}>
+          <BackIcon width="44" height="44" fill="#fff" />
+        </BackButton>
+        <HeaderTitle>ESCOLHA UM PROFISSIONAL</HeaderTitle>
+      </HeaderArea>
+      {loading && <LoadingIcon size="large" color="#FFFFFF" />}
+
+      <ListArea>
+        {listBarbers.map((item, k) => (
+          <BarberItem key={k} data={item} />
+        ))}
+      </ListArea>
     </Container>
   );
 };
--- src/screens/Agendar/styles.js
+++ src/screens/Agendar/styles.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.ScrollView`
   background-color: #111;
   flex: 1;
-  justify-content: center;
-  align-items: center;
 `;
-
 export const TextTittle = styled.Text`
   text-align: center;
   color: #fff;
@@ -14,4 +11,43 @@ export const TextTittle = styled.Text`
   font-weight: bold;
   margin: 5px;
   margin-top: 10px;
+`;
+export const ImagePreloadlarge = styled.Image`
+  width: 70%;
+  height: 260px;
+  align-items: center;
+  justify-content: center;
+  margin-top: -170px;
+`;
+export const BackButton = styled.TouchableHighlight`
+  position: absolute;
+  left: 0;
+  top: 30px;
+  z-index: 9;
+`;
+
+export const HeaderArea = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 25px;
+  margin-bottom: -40px;
+  justify-content: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: 15px;
+  color: #fff;
+  font-weight: bold;
+  margin: 15px;
+  text-align: center;
+`;
+export const ListArea = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+  flex: 1;
+  margin-top: 30px;
+`;
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
 `;
