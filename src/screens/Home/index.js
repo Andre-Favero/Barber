ee3be14fec0c6b8aec46b4f404fb789945ab02cc
André Favero <andrefaverofacul@gmail.com>
2023-01-27 16:09:40 -0300

    fix: Remoção dos barbeiros
    Removido a parte de request e render da lista da barbeiros da homepage e realoca para a pagina de equipe

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,44 +3,13 @@ import * as C from './styles';
 import HomeButtons from '../../components/HomeButtons';
 import ComodidadesButtons from '../../components/ComodidadesButtons';
 import AboutUs from '../../components/AboutUs';
-import {HeaderArea, HeaderTitle, LoadingIcon, ListArea} from './styles';
 import api from '../../api';
-import BarberItem from '../../components/BarberItem';
 import {Alert, RefreshControl} from 'react-native';
 
 export default () => {
   const [locationText, setLocationText] = useState('');
-  const [coords, setCoords] = useState();
   const [loading, setLoading] = useState(false);
-  const [listBarbers, setListaBarbers] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
-
-  const getBarbers = async () => {
-    setLoading(true);
-    setListaBarbers([]);
-    let lat = null;
-    let lng = null;
-    if (coords) {
-      lat = coords.latitude;
-      lng = coords.longitude;
-    }
-
-    let res = await api.getBarbers(lat, lng, locationText);
-    if (res.error === '') {
-      if (res.loc) {
-        setLocationText('');
-      }
-      setListaBarbers(res.data);
-    } else {
-      Alert.alert(res.error);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getBarbers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const onRefresh = () => {
     setRefreshing(false);
