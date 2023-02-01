638aa400b0a31dfd53503ce8b6eb9d325f7bcea0
André Favero <andrefaverofacul@gmail.com>
2023-02-01 08:37:43 -0300

    feat: Conexão com firebase

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './src/stacks/MainStack';
+import messaging from '@react-native-firebase/messaging';
 import UserContextProvider from './src/contexts/UserContext';
 import RNBootSplash from 'react-native-bootsplash';
 
 export default () => {
+  useEffect(() => {
+    async function requestUserPermission() {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    }
+    requestUserPermission();
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('Foreground ', remoteMessage);
+    });
+    return unsubscribe;
+  }, []);
+
   const getNavigationTheme = () => ({
     dark: true,
     colors: {
